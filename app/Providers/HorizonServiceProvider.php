bfe1b69cc833c06137ffe97b16aa0b8d9a97dc8a
Nay Thu Khant <naythukhant644@gmail.com>
2022-09-15 08:20:47 +0630

    Horizon Night Mode added

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -20,8 +20,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         // Horizon::routeSmsNotificationsTo('15556667777');
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
-
-        // Horizon::night();
+        Horizon::night();
     }
 
     /**
